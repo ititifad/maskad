8a45af676b4faeb8beb1f28f2556c88e1aef931f
ititifad <dex90jay@gmail.com>
2023-04-16 22:26:15 +0300

    postgres connection

--- MaskadWebsite/settings.py
+++ MaskadWebsite/settings.py
@@ -76,12 +76,24 @@ WSGI_APPLICATION = 'MaskadWebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'XbiKtUGPuIXZHl9O4ZrU',
+        'HOST': 'containers-us-west-154.railway.app',
+        'PORT': '6013',
     }
 }
+
 
 
 # Password validation
